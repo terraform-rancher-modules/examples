bfee8feb2a04aacf7854f70db7a9716aba563899
Murali Paluru <leodotcloud@gmail.com>
2023-05-18 11:53:39 +0530

    fixes

--- rancher-on-aws/main.tf
+++ rancher-on-aws/main.tf
@@ -1,38 +1,60 @@
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region = var.aws_region
+}
+
 module "upstream-cluster" {
-  source = "../terraform-infra-aws"
-  aws_access_key = var.aws_secret_key
-  aws_secret_key = var.aws_secret_key
+  source = "../../terraform-infra-aws"
+  providers = {
+    aws = aws
+  }
+
   prefix = var.prefix
-  instance_count = 1
+  instance_count = var.instance_count
   instance_ssh_key_name = "leodotcloud"
   ssh_private_key_path = "~/.ssh/id_ed25519"
   #  should_create_security_group = false
 }
 
 module "rke" {
-  source = "../terraform-rke-cluster"
+  source = "../../terraform-rke-cluster"
   depends_on = [module.upstream-cluster]
 
   node_username = "ubuntu"
   ssh_private_key_pem = "~/.ssh/id_ed25519"
 
-  rancher_nodes = [
+  rancher_nodes = [for instance_ips in module.upstream-cluster.instances_ips:
     {
-      public_ip = module.upstream-cluster.instances_public_ip[0],
-      private_ip = module.upstream-cluster.instances_private_ip[0],
+      public_ip = instance_ips.public_ip,
+      private_ip = ""
+#      private_ip = instance_ips.private_ip,
       roles = ["etcd", "controlplane", "worker"]
     }
   ]
 }
 
+provider "helm" {
+  kubernetes {
+    config_path = "${path.module}/${module.rke.rke_kubeconfig_filename}"
+  }
+}
+
+provider "kubernetes" {
+  config_path = "${path.module}/${module.rke.rke_kubeconfig_filename}"
+  insecure    = true
+}
+
 module "rancher_install" {
-  source = "../terraform-rancher-install"
+  source = "../../terraform-rancher-install"
   depends_on = [module.rke]
 
   rancher_hostname = join(".", ["rancher", module.upstream-cluster.instances_public_ip[0], "sslip.io"])
   rancher_replicas  = 1
-  rancher_additional_helm_values = [
-    "bootstrapPassword: changeme",
-  ]
-  kubeconfig_file = "./kube_config_cluster.yml"
+  rancher_bootstrap_password = "changeme"
+
+  providers = {
+    kubernetes = kubernetes
+    helm = helm
+  }
 }
--- rancher-on-aws/variables.tf
+++ rancher-on-aws/variables.tf
@@ -8,9 +8,26 @@ variable "aws_secret_key" {
   description = "AWS secret key used to create AWS infrastructure"
 }
 
+variable "aws_region" {
+  type        = string
+  description = "AWS region used for all resources"
+}
+
 # Required
 variable "prefix" {
   type        = string
   description = "Prefix added to names of all resources"
+}
+
+#variable "kubeconfig_file" {
+#  description = "The kubeconfig to use to interact with the cluster"
+#  default     = "~/.kube/config"
+#  type        = string
+#}
+
+variable "instance_count" {
+  type    = number
+  description = "Number of EC2 instances to create"
+  default = 3
 }
 
